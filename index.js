c08fafaf5a7bd418e539ca984bccebb3f87234ce
larkindsmith <larkindsmith@gmail.com>
2019-01-15 18:46:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function dwarfRollCall(dwarves) {
-  
+  let empty = ''
+  for (let i = 0; i < dwarves.length; i++) {
+    let counter = i + 1
+    empty = `${counter}. + ${dwarves[i]}`
+    
+  }
 }
 
 function summonCaptainPlanet(planeteerCalls){
